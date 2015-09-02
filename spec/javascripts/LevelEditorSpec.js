79d6e9b6721ca2dcb254c9432eedca434a81447f
ruth.wall <ruth.wall@outlook.com>
2015-09-02 19:32:58 +0100

    #544 Check for the existence of click handler function

--- spec/javascripts/LevelEditorSpec.js
+++ spec/javascripts/LevelEditorSpec.js
@@ -3,8 +3,13 @@
  */
 
 describe("Level Editor AddRemoveRoad", function() {
+
     it('exists', function(){
         expect(ocargo.LevelEditor.AddRemoveRoad).toBeDefined();
+    });
+
+    it('has a click handler function', function(){
+        expect(ocargo.LevelEditor.AddRemoveRoad.handleMouseDown).toBeDefined();
     });
 
 });
